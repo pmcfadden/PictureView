2ca04793a3296df7d1b95b6ea91cd75279ca1545
Patrick McFadden <pmcfadde@thoughtworks.com>
2011-05-23 21:59:59 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PictureView::Application.config.secret_token = 'dcf8149ea266db5e12ecd65229dd9624ba2a7e9fc893d1b4dbe83ea6b30c736bee05535c16c757ee075ec9efa8a06a2f245e9d51c7b881e5ee0211c9f447cbf0'
